5be1cc18d2cab21d17ed1b358b44c3a792889cc7
snandakumar87 <snandaku@redhat.com>
2021-07-14 16:15:20 -0400

    transaction-monitor

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "15ccf201-dc1e-4732-8346-c155ff96a49e";
+private static final String ID = "ec26071f-80e8-4923-8d9f-69a6d6e2cb5f";
 }
