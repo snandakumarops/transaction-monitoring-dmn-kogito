bfb7522f5f256fb336aa662e8e60ff16df0f7006
snandakumar87 <snandaku@redhat.com>
2021-07-18 21:37:13 -0400

    Added small rye health

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "ec26071f-80e8-4923-8d9f-69a6d6e2cb5f";
+private static final String ID = "d3798574-243f-4084-aacc-806d33d1d71a";
 }
